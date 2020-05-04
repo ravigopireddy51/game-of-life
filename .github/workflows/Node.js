2544658d16da38739ce520739db1bd76a499eb6f
ravigopireddy51 <57981645+ravigopireddy51@users.noreply.github.com>
2020-05-04 18:51:09 +0530

    Create Node.js

---
+++ .github/workflows/Node.js
@@ -0,0 +1,35 @@
+on: [push]
+
+name: Linux_Container_Node_Workflow
+
+jobs:
+  build-and-deploy:
+    runs-on: ubuntu-latest
+    steps:
+    # checkout the repo
+    - name: 'Checkout GitHub Action' 
+      uses: actions/checkout@master
+    
+    - name: 'Login via Azure CLI'
+      uses: azure/login@v1
+      with:
+        creds: ${{ secrets.AZURE_CREDENTIALS }}
+    
+    - uses: azure/docker-login@v1
+      with:
+        login-server: contoso.azurecr.io
+        username: ${{ secrets.REGISTRY_USERNAME }}
+        password: ${{ secrets.REGISTRY_PASSWORD }}
+    
+    - run: |
+        docker build . -t contoso.azurecr.io/gameoflife-image:${{ github.sha }}
+        docker push contoso.azurecr.io/gameoflife-image:${{ github.sha }} 
+      
+    - uses: azure/webapps-container-deploy@v1
+      with:
+        app-name: 'node-rnc'
+        images: 'contoso.azurecr.io/gameoflife-image:${{ github.sha }}'
+    
+    - name: Azure logout
+      run: |
+        az logout
